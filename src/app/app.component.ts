48134907069550e824fb56bdd89e6b4c58ac7e35
Sujitraj Thirumurthy <sujitrajt@gmail.com>
2019-11-08 00:12:18 +0530

    angularTo-do App

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  todoArray = [];
+  addTodo(value) {
+    this.todoArray.push(value);
+    //console.log(this.todos);
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
+  title = "todo-App";
+}
